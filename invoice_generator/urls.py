84c6aff59c620082956a4703e895c5d43c8337ff
sagar <sagarpawar606@gmail.com>
2022-08-01 10:40:08 +0530

    base.urls changed

--- invoice_generator/urls.py
+++ invoice_generator/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
     path('', include('base.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG == True:
+    urlpatterns.append(path('admin/', admin.site.urls),)
