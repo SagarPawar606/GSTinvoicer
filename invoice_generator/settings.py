93ada4cbed1e6e83955e02681be0088966d49505
sagar <sagarpawar606@gmail.com>
2022-11-29 10:25:37 +0530

    railway domain updated

--- invoice_generator/settings.py
+++ invoice_generator/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'django-gst.herokuapp.com','gstinvoicer-production.up.railway.app/']
+ALLOWED_HOSTS = ['127.0.0.1', 'django-gst.herokuapp.com','gstinvoicer-production.up.railway.app']
 
 
 # Application definition
