f4404102e7fbfc8ef203b19caca95b4fc01ff450
Sagar Pawar <sagarpawar606@gmail.com>
2022-12-04 09:31:02 +0530

    Update settings.py

--- invoice_generator/settings.py
+++ invoice_generator/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECRET_KEY = 'django-insecure-7i6o4ru0k!)oe4e^2$s6c0%!nj)iqo$yxipm8)p*gdk@3of-wb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'django-gst.herokuapp.com','gstinvoicer-production.up.railway.app']
 
